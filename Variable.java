dd8932abab21a4ff361a540cad2e63e194df167e
D-Chernov <dimm1996@list.ru>
2022-01-18 20:59:56 +0300

    add variable

---
+++ Variable.java
@@ -0,0 +1,31 @@
+/*package com.company;
+import java.lang.String;*/
+
+public class Variable {
+	public static void main(String[] args) {
+		byte q = 100;
+		short w = 2000;
+		int e = 300000;
+		long r = 4000000;
+		float t = 3.14f;
+		double y = 6.28d;
+		char u = 'R';
+		boolean i = false;
+		System.out.print(q);
+		System.out.print(" ");
+		System.out.print(w);
+		System.out.print(" ");
+		System.out.print(e);
+		System.out.print(" ");
+		System.out.print(r);
+		System.out.print(" ");
+		System.out.print(t);
+		System.out.print(" ");
+		System.out.print(y);
+		System.out.print(" ");
+		System.out.print(u);
+		System.out.print(" ");
+		System.out.print(i);
+
+	}
+}
