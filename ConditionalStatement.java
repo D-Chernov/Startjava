de02115fab9d8755da740be9012ec469c9e0e332
D-Chernov <dimm1996@list.ru>
2022-01-19 21:58:35 +0300

    Сделал 7 задание

---
+++ ConditionalStatement.java
@@ -0,0 +1,27 @@
+public class ConditionalStatement{
+	public static void main(String[] args) {
+		char bukva = 'I'; 
+		if (bukva =='M') {
+			System.out.println("Your name is Mark");
+		} else if (bukva =='I') {
+			System.out.println("Your name is Inna");
+		} else {
+			System.out.println("Your name is Win");
+		}
+		int are = 30;
+		if (are>20) {
+			System.out.println("Vam mogno tyt but");
+		} char pol = 'M';
+		if (pol == 'M') {
+			System.out.println("Prohodi");
+		} char pol2 = 'G';
+		if (pol2 != 'G') {
+			System.out.println("Welcome");
+		} float rost = 1.85f;
+		if (rost < 1.80) { 
+			System.out.println("you small");
+		} else {
+			System.out.println("you big");
+		}
+	}
+}
